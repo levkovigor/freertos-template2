97a79b29da01a4cbac7f6d4e8d623898da9e0f29
Robin.Mueller <robin.mueller.m@gmail.com>
2020-08-19 19:46:47 +0200

    important defines for nand flash added

--- bootloader/core/watchdog.c
+++ bootloader/core/watchdog.c
@@ -10,7 +10,6 @@
 #include <hal/Timing/WatchDogTimerNoOS.h>
 
 uint32_t watchdog_last_fed_ms_count = 0;
-uint32_t debug_counter = 0;
 
 void initiate_external_watchdog() {
     WDT_start();
@@ -20,13 +19,8 @@ void initiate_external_watchdog() {
 }
 
 void feed_watchdog_if_necessary(void) {
-
     if(u32_ms_counter - watchdog_last_fed_ms_count >= WATCHDOG_FEED_PERIOD_MS) {
         WDT_forceKick();
-        debug_counter ++;
-        if(debug_counter == 100) {
-            printf("watchdog being kicked\n\r");
-        }
         watchdog_last_fed_ms_count = u32_ms_counter;
     }
 }
