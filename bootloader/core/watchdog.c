27f61d218d03ea23aacb4f291df814c8d7c9fb51
Robin.Mueller <robin.mueller.m@gmail.com>
2020-08-19 14:36:42 +0200

    small correction

--- bootloader/core/watchdog.c
+++ bootloader/core/watchdog.c
@@ -17,7 +17,7 @@ void initiate_external_watchdog() {
 }
 
 void feed_watchdog_if_necessary(void) {
-    if(u32_ms_counter - watchdog_last_fed_ms_count > WATCHDOG_FEED_PERIOD_MS) {
+    if(u32_ms_counter - watchdog_last_fed_ms_count >= WATCHDOG_FEED_PERIOD_MS) {
         WDT_forceKick();
     }
 }
