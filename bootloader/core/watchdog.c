0dc202bbe46dfac4cda5e8d592c62f414ce7a868
Robin.Mueller <robin.mueller.m@gmail.com>
2020-08-19 14:35:41 +0200

    slight improvement to watchdog

--- bootloader/core/watchdog.c
+++ bootloader/core/watchdog.c
@@ -12,12 +12,12 @@ uint32_t watchdog_last_fed_ms_count = 0;
 void initiate_external_watchdog() {
     WDT_start();
     WDT_forceKick();
-    // Now, we need to ensure the watchdog is kicked every 10 ms
+    // Now, we need to ensure the watchdog is fed often enough but not too often
     watchdog_last_fed_ms_count = u32_ms_counter;
 }
 
 void feed_watchdog_if_necessary(void) {
-    if(u32_ms_counter - watchdog_last_fed_ms_count > 10) {
+    if(u32_ms_counter - watchdog_last_fed_ms_count > WATCHDOG_FEED_PERIOD_MS) {
         WDT_forceKick();
     }
 }
