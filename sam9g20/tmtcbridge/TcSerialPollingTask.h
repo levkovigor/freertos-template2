5868765d383ac5d2a4110fcda4af915fbb6c9189
Robin.Mueller <robin.mueller.m@gmail.com>
2020-08-21 00:28:30 +0200

    new polling task with dle encoding working

--- sam9g20/tmtcbridge/TcSerialPollingTask.h
+++ sam9g20/tmtcbridge/TcSerialPollingTask.h
@@ -5,10 +5,8 @@
 #include <fsfw/tasks/ExecutableObjectIF.h>
 #include <fsfw/ipc/MessageQueueIF.h>
 #include <fsfw/storagemanager/StorageManagerIF.h>
-#include <fsfw/container/SimpleRingBuffer.h>
-#include <fsfw/container/FIFO.h>
 #include <fsfw/container/SharedRingBuffer.h>
-#include <sam9g20/tmtcbridge/PusParser.h>
+#include <fsfw/osal/FreeRTOS/BinarySemaphore.h>
 
 extern "C" {
 #include <board.h>
@@ -39,12 +37,8 @@ public:
     static constexpr uint8_t TC_RECEPTION_QUEUE_DEPTH = 10;
     /** The frame size will be set to this value if no other value is
      *  supplied in the constructor. */
-//    static constexpr uint16_t MAX_RECEPTION_BUFFER_SIZE = 2048;
-    static constexpr uint16_t DMA_BUCKET_READ_SIZE = 20;
-    static constexpr float DEFAULT_SERIAL_TIMEOUT_BAUDTICKS = 20;
-    //! 12 bytes is minimal size of a PUS packet, which is used in the ctor
-    //! to calculate this value.
-    //size_t maxNumberOfStoredPackets;
+    static constexpr uint16_t SERIAL_FRAME_MAX_SIZE = 1500;
+    static constexpr float DEFAULT_SERIAL_TIMEOUT_BAUDTICKS = 5;
 
     /**
      * Default ctor.
@@ -82,12 +76,28 @@ private:
 	UARTconfig configBus0 = { .mode = AT91C_US_USMODE_NORMAL |
 			AT91C_US_CLKS_CLOCK | AT91C_US_CHRL_8_BITS | AT91C_US_PAR_NONE |
 			AT91C_US_OVER_16 | AT91C_US_NBSTOP_1_BIT, .baudrate = 115200,
-			.timeGuard = 1, .busType = rs232_uart, .rxtimeout = 0xFFFF };
+			.timeGuard = 0, .busType = rs232_uart, .rxtimeout = 0xFFFF };
+
+	bool started = false;
+
+	UARTgenericTransfer uartTransfer1;
+	static volatile uint8_t transfer1bytesReceived;
+	BinarySemaphore uartSemaphore1;
+	UARTtransferStatus transfer1Status = done_uart;
+	std::array<uint8_t, SERIAL_FRAME_MAX_SIZE> readBuffer1;
+
+	BinarySemaphore uartSemaphore2;
+	static volatile uint8_t transfer2bytesReceived;
+	UARTgenericTransfer uartTransfer2;
+	UARTtransferStatus transfer2Status = done_uart;
+	std::array<uint8_t, SERIAL_FRAME_MAX_SIZE> readBuffer2;
+
 
 	/**
 	 * @brief 	This function handles reading UART data in a permanent loop
 	 * @details
 	 */
+	void initiateUartTransfers();
 	void pollUart();
 	ReturnValue_t pollTc();
 
@@ -103,23 +113,11 @@ private:
 	 */
 	ReturnValue_t handleOverrunError();
 
-//	//! Variable sized container to have flexible frame sizes.
-//	std::vector<uint8_t> recvBuffer;
-//	/** Will be used in read call to driver to determine how many bytes
-//	 * are read. Do not change after initialization! */
-//	size_t frameSize;
-	//MessageQueueId_t targetTcDestination = MessageQueueIF::NO_QUEUE;
-	//PusParser* pusParser = nullptr;
-	//	void handleSuccessfulTcRead();
-//	/**
-//	 * This function forwards the found PUS packets to the FSFW software bus.
-//	 * Critical section because the next read function should be called ASAP.
-//	 */
-//	void handleTc();
-//	void transferPusToSoftwareBus(uint16_t recvBufferIndex,
-//			uint16_t packetSize);
-//	void printTelecommand(uint8_t* tcPacket, uint16_t packetSize);
-
+	static void uart1Callback(SystemContext context, xSemaphoreHandle sem);
+	static void uart2Callback(SystemContext context, xSemaphoreHandle sem);
+	static void genericUartCallback(SystemContext context,
+			xSemaphoreHandle sem);
 };
+
 
 #endif /* SAM9G20_TMTCBRIDGE_SERIALPOLLINGTASK_H_ */
