d47586b4014644229d7b78192a123cabbc0a272d
Robin Mueller <robin.mueller.m@gmail.com>
2020-09-04 09:51:38 +0200

    added software image handler

---
+++ sam9g20/core/SoftwareImageHandler.cpp
@@ -0,0 +1,5 @@
+#include "SoftwareImageHandler.h"
+
+SoftwareImageHandler::SoftwareImageHandler(object_id_t objectId):
+		SystemObject(objectId) {
+}
---
+++ sam9g20/core/SoftwareImageHandler.h
@@ -0,0 +1,14 @@
+#ifndef SAM9G20_CORE_SOFTWAREIMAGEHANDLER_H_
+#define SAM9G20_CORE_SOFTWAREIMAGEHANDLER_H_
+#include <fsfw/objectmanager/SystemObject.h>
+
+class SoftwareImageHandler: public SystemObject {
+public:
+	SoftwareImageHandler(object_id_t objectId);
+private:
+
+};
+
+
+
+#endif /* SAM9G20_CORE_SOFTWAREIMAGEHANDLER_H_ */
