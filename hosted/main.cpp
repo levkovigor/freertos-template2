857c94be529b99453448d1969421f87efcd2ea5e
Robin.Mueller <robin.mueller.m@gmail.com>
2020-09-06 12:36:20 +0200

    added printouts

--- hosted/main.cpp
+++ hosted/main.cpp
@@ -2,10 +2,24 @@
 #include <gcov.h>
 #endif
 
+#include <iostream>
 
 // This will be the entry to the mission specific code
 void initMission();
 
+#ifdef WIN32
+static const char* COMPILE_PRINTOUT = "Windows";
+#elif LINUX
+static const char* COMPILE_PRINTOUT = "Linux";
+#endif
+
+#ifndef SW_VERSION
+#define SW_VERSION 0
+#endif
+
+#ifndef SW_SUBVERSION
+#define SW_SUBVERSION 0
+#endif
 
 /**
  * @brief 	This is the main program for the hosted build. It can be run for
@@ -14,6 +28,12 @@ void initMission();
  */
 int main(void)
 {
+    std::cout << "-- SOURCE Hosted OBSW --" << std::endl;
+    std::cout << "-- Compiled for " << COMPILE_PRINTOUT << " --" << std::endl;
+    std::cout << "-- Software version v" << SW_VERSION << "." << SW_SUBVERSION
+            << " -- " << std::endl;
+    std::cout << "-- " <<  __DATE__ << " "
+            << __TIME__ << " --" << std::endl;
 	initMission();
 	for(;;) {}
 }
