d942507cb4c1935b198b4bccd379651d8a1df653
Robin.Mueller <robin.mueller.m@gmail.com>
2020-09-19 19:35:59 +0200

    srv23 taken over

--- mission/pus/Service23FileManagement.cpp
+++ mission/pus/Service23FileManagement.cpp
@@ -7,65 +7,228 @@
 
 #include <mission/pus/Service23FileManagement.h>
 
-#include <apid.h>
-#include <pusIds.h>
+#include <config/tmtc/apid.h>
+#include <config/tmtc/pusIds.h>
 
-#include <fsfw/serviceinterface/ServiceInterfaceStream.h>
-#include <fsfw/tmtcpacket/pus/TmPacketStored.h>
-#include <fsfw/action/ActionMessage.h>
-#include <fsfw/memory/AcceptsMemoryMessagesIF.h>
-#include <fsfw/ipc/QueueFactory.h>
+#include <framework/serviceinterface/ServiceInterfaceStream.h>
+#include <config/objects/systemObjectList.h>
+#include <framework/tmtcpacket/pus/TmPacketStored.h>
+#include <framework/memory/HasFileSystemIF.h>
+#include <framework/memory/FileSystemMessage.h>
+#include <framework/action/ActionMessage.h>
 
 Service23FileManagement::Service23FileManagement(object_id_t objectId_):
-		PusServiceBase(objectId_,apid::SOURCE_OBSW,pus::PUS_SERVICE_23),
-		packetSubCounter(0), fileSystemHandlerQueueId(0)  {
-	commandQueue = QueueFactory::instance()->createMessageQueue(commandQueueDepth);
-	IPCStore = objectManager->get<StorageManagerIF>(objects::IPC_STORE);
+CommandingServiceBase(objectId_,apid::SOURCE_OBSW,pus::PUS_SERVICE_23,
+								4,60,objects::PUS_PACKET_DISTRIBUTOR,objects::PUS_FUNNEL) {
 }
+
 
 Service23FileManagement::~Service23FileManagement() {
-	QueueFactory::instance()->deleteMessageQueue(commandQueue);
 }
 
-ReturnValue_t Service23FileManagement::handleRequest(uint8_t subservice) {
-	switch(currentPacket.getSubService()){
-	case Subservice::CREATE_FILE: {
-		//identify queue of file system handler
-		AcceptsMemoryMessagesIF* fileSystemHandlerObject =
-		        objectManager->get<AcceptsMemoryMessagesIF>(fileSystemHandler);
-		if(fileSystemHandlerObject!=NULL){
-			fileSystemHandlerQueueId = fileSystemHandlerObject->getCommandQueue();
-		}
-		else {
-			sif::error << "No target queue to file system handler found" << std::endl;
+
+ReturnValue_t Service23FileManagement::isValidSubservice(uint8_t subservice) {
+	switch(subservice){
+		case Subservice::CREATE_FILE:
+			sif::info << "Service 23 detected valid subservice" << std::endl;
+			return HasReturnvaluesIF::RETURN_OK;
+		case Subservice::DELETE_FILE:
+			return HasReturnvaluesIF::RETURN_OK;
+		case Subservice::CREATE_DIRECTORY:
+			return HasReturnvaluesIF::RETURN_OK;
+		case Subservice::DELETE_DIRECTORY:
+			return HasReturnvaluesIF::RETURN_OK;
+		case Subservice::WRITE:
+			return HasReturnvaluesIF::RETURN_OK;
+		case Subservice::READ:
+			return HasReturnvaluesIF::RETURN_OK;
+		default:
 			return HasReturnvaluesIF::RETURN_FAILED;
-		}
-
-		store_address_t storeId;
-		ReturnValue_t result = IPCStore->addData(&storeId,
-		        currentPacket.getApplicationData(), currentPacket.getApplicationDataSize());
-		if(result!=HasReturnvaluesIF::RETURN_OK){
-			sif::error << "Adding data to IPC store failed" << std::endl;
-			return RETURN_FAILED;
-		}
-
-		CommandMessage command;
-		ActionMessage::setCommand(&command, Subservice::CREATE_FILE, storeId);
-		result = commandQueue->sendMessage(fileSystemHandlerQueueId, &command);
-		if(result != HasReturnvaluesIF::RETURN_OK){
-			sif::error << " MessageQueue of SDCardHandler is full!" << std::endl;
-		}
-		return HasReturnvaluesIF::RETURN_OK;
 	}
-	case Subservice::DELETE_FILE: {
-		return RETURN_OK;
+}
+
+
+ReturnValue_t Service23FileManagement::getMessageQueueAndObject(
+		uint8_t subservice, const uint8_t *tcData, size_t tcDataLen,
+		MessageQueueId_t *id, object_id_t *objectId) {
+
+	ReturnValue_t result = checkAndAcquireTargetID(objectId, tcData, tcDataLen);
+	if (result != RETURN_OK) {
+		return result;
+	}
+
+	result = checkInterfaceAndAcquireMessageQueue(id, objectId);
+	return result;
+}
+
+
+ReturnValue_t Service23FileManagement::checkAndAcquireTargetID(object_id_t* objectIdToSet, const uint8_t* tcData, uint32_t tcDataLen) {
+	size_t size = tcDataLen;
+	if(SerializeAdapter<object_id_t>::deSerialize(objectIdToSet,&tcData,&size,true)!=HasReturnvaluesIF::RETURN_OK)
+		return CommandingServiceBase::INVALID_TC;
+	 else
+		 return HasReturnvaluesIF::RETURN_OK;
+}
+
+
+ReturnValue_t Service23FileManagement::checkInterfaceAndAcquireMessageQueue(MessageQueueId_t* MessageQueueToSet, object_id_t* objectId) {
+	// check hasActionIF property of target
+	HasFileSystemIF* possibleTarget = objectManager->get<HasFileSystemIF>(*objectId);
+	if(possibleTarget!=NULL){
+		*MessageQueueToSet = possibleTarget->getCommandQueue();
+		return HasReturnvaluesIF::RETURN_OK;
+	}else {
+		return CommandingServiceBase::INVALID_OBJECT;
+	}
+}
+
+
+ReturnValue_t Service23FileManagement::prepareCommand(CommandMessage* message,
+		uint8_t subservice, const uint8_t *tcData, size_t tcDataLen,
+		uint32_t *state, object_id_t objectId) {
+	ReturnValue_t result;
+	if(subservice == Subservice::CREATE_FILE){
+
+	}
+	if(subservice == Subservice::DELETE_FILE){
+		result = prepareDeleteFileCommand(message, tcData, tcDataLen);
+	}
+	else if(subservice == Subservice::CREATE_DIRECTORY){
+		result = prepareCreateDirectoryCommand(message, tcData, tcDataLen);
+	}
+	else if(subservice == Subservice::DELETE_DIRECTORY){
+		result = prepareDeleteDirectoryCommand(message, tcData, tcDataLen);
+	}
+	else if(subservice ==Subservice::WRITE){
+		result = prepareWriteCommand(message, tcData, tcDataLen);
+	}
+	else if(subservice ==Subservice::READ){
+		result = prepareReadCommand(message, tcData, tcDataLen);
+	}
+	else {
+		result = RETURN_FAILED;
+	}
+
+	return result;
+
+}
+
+
+ReturnValue_t Service23FileManagement::prepareDeleteFileCommand(
+		CommandMessage *message, const uint8_t *tcData, uint32_t tcDataLen) {
+	store_address_t parameterAddress;
+	/* Add data without the objectId to the IPC store */
+	ReturnValue_t result = IPCStore->addData(&parameterAddress, tcData + sizeof(object_id_t), tcDataLen - sizeof(object_id_t));
+	if(result == HasReturnvaluesIF::RETURN_OK){
+		FileSystemMessage::setDeleteFileCommand(message, parameterAddress);
+	}
+	else{
+		sif::info << "Failed to add data to IPC Store" << std::endl;
+		return HasReturnvaluesIF::RETURN_FAILED;
+	}
+	return HasReturnvaluesIF::RETURN_OK;
+}
+
+
+ReturnValue_t Service23FileManagement::prepareCreateDirectoryCommand(
+		CommandMessage *message, const uint8_t *tcData, uint32_t tcDataLen) {
+	store_address_t parameterAddress;
+	/* Add data without the objectId to the IPC store */
+	ReturnValue_t result = IPCStore->addData(&parameterAddress, tcData + sizeof(object_id_t), tcDataLen - sizeof(object_id_t));
+	if(result == HasReturnvaluesIF::RETURN_OK){
+		FileSystemMessage::setCreateDirectoryCommand(message, parameterAddress);
+	}
+	else{
+		sif::info << "Failed to add data to IPC Store" << std::endl;
+		return HasReturnvaluesIF::RETURN_FAILED;
+	}
+	return HasReturnvaluesIF::RETURN_OK;
+}
+
+
+ReturnValue_t Service23FileManagement::prepareDeleteDirectoryCommand(
+		CommandMessage *message, const uint8_t *tcData, uint32_t tcDataLen) {
+	store_address_t parameterAddress;
+	/* Add data without the objectId to the IPC store */
+	ReturnValue_t result = IPCStore->addData(&parameterAddress, tcData + sizeof(object_id_t), tcDataLen - sizeof(object_id_t));
+	if(result == HasReturnvaluesIF::RETURN_OK){
+		FileSystemMessage::setDeleteDirectoryCommand(message, parameterAddress);
+	}
+	else{
+		sif::info << "Failed to add data to IPC Store" << std::endl;
+		return HasReturnvaluesIF::RETURN_FAILED;
+	}
+	return HasReturnvaluesIF::RETURN_OK;
+}
+
+
+ReturnValue_t Service23FileManagement::prepareWriteCommand(
+		CommandMessage *message, const uint8_t *tcData,
+		uint32_t tcDataLen) {
+
+	// store additional parameters into the Inter Process Communication Store
+	store_address_t parameterAddress;
+	ReturnValue_t result = IPCStore->addData(&parameterAddress, tcData + sizeof(object_id_t), tcDataLen - sizeof(object_id_t));
+	if(result == HasReturnvaluesIF::RETURN_OK){
+		// setWriteCommand expects a Command Message and a store address pointing to the write packet
+		FileSystemMessage::setWriteCommand(message, parameterAddress);
+	}
+	else{
+		sif::info << "Failed to add data to IPC Store" << std::endl;
+		return HasReturnvaluesIF::RETURN_FAILED;
+	}
+	return HasReturnvaluesIF::RETURN_OK;
+}
+
+
+ReturnValue_t Service23FileManagement::prepareReadCommand(
+		CommandMessage *message, const uint8_t *tcData,
+		uint32_t tcDataLen) {
+
+	store_address_t parameterAddress;
+	ReturnValue_t result = IPCStore->addData(&parameterAddress, tcData + sizeof(object_id_t),
+			tcDataLen - sizeof(object_id_t));
+	if (result == HasReturnvaluesIF::RETURN_OK) {
+		FileSystemMessage::setReadCommand(message, parameterAddress);
+	} else {
+		sif::info << "Failed to add data to IPC Store" << std::endl;
+		return HasReturnvaluesIF::RETURN_FAILED;
+	}
+	return HasReturnvaluesIF::RETURN_OK;
+}
+
+
+ReturnValue_t Service23FileManagement::handleReply(const CommandMessage* reply,
+		Command_t previousCommand, uint32_t* state,
+		CommandMessage* optionalNextCommand, object_id_t objectId,
+		bool* isStep) {
+	Command_t reply_id = reply->getCommand();
+	ReturnValue_t result;
+	switch(reply_id)  {
+	case FileSystemMessage::COMPLETION_SUCCESS: {
+		result = EXECUTION_COMPLETE;
+		break;
+	}
+	case FileSystemMessage::READ_REPLY: {
+		store_address_t storeId = FileSystemMessage::getStoreId(reply);
+		size_t size = 0;
+		const uint8_t * buffer = NULL;
+		result = IPCStore->getData(storeId, &buffer, &size);
+		if(result != RETURN_OK) {
+			sif::error << "Service 23: Could not retrieve data for data reply";
+			return result;
+		}
+		sendTmPacket(Subservice::READ_REPLY, objectId, buffer, size);
+		result = IPCStore ->deleteData(storeId);
+		break;
+	}
+	case FileSystemMessage::COMPLETION_FAILED: {
+		result = HasReturnvaluesIF::RETURN_FAILED;
+		break;
 	}
 	default:
-		return AcceptsTelecommandsIF::INVALID_SUBSERVICE;
+		result = INVALID_REPLY;
 	}
-}
-
-ReturnValue_t Service23FileManagement::performService() {
-	return HasReturnvaluesIF::RETURN_OK;
+	return result;
 }
 
--- mission/pus/Service23FileManagement.h
+++ mission/pus/Service23FileManagement.h
@@ -8,8 +8,8 @@
 #ifndef MISSION_PUS_SERVICE23FILEMANAGEMENT_H_
 #define MISSION_PUS_SERVICE23FILEMANAGEMENT_H_
 
-#include <fsfw/tmtcservices/PusServiceBase.h>
-#include <config/objects/systemObjectList.h>
+#include <framework/objectmanager/SystemObject.h>
+#include <framework/tmtcservices/CommandingServiceBase.h>
 
 /**
  * \brief File Management Service
@@ -17,7 +17,7 @@
  * Full Documentation: ECSS-E-ST-70-41C p.403
  *
  * The file management service provides the capability to manage on-board file
- * systems and files
+ * systems and files. Not all subservices listed below are yet implemented.
  * Service capability:
  *   - TC[23,1]: Create a file
  *   - TC[23,2]: Delete a file
@@ -39,30 +39,91 @@
  *
  * \ingroup pus_services
  */
-class Service23FileManagement: public PusServiceBase {
+class Service23FileManagement: public CommandingServiceBase {
 public:
-	// Custom events which can be triggered
-	static const uint8_t SUBSYSTEM_ID = SUBSYSTEM_ID::PUS_SERVICE_23;
-	static const Event TEST2 = MAKE_EVENT(0, SEVERITY::INFO);
-
-	enum Subservice {
-		CREATE_FILE = 1,
-		DELETE_FILE = 2,
-		REPORT_ATTRIBUTES = 3
-	};
 
 	Service23FileManagement(object_id_t objectId_);
 	virtual ~Service23FileManagement();
-	ReturnValue_t handleRequest(uint8_t subservice) override;
-	ReturnValue_t performService() override;
-private:
-	uint16_t packetSubCounter;
-	object_id_t fileSystemHandler = objects::SD_CARD_HANDLER;
-	MessageQueueId_t fileSystemHandlerQueueId;
-	MessageQueueIF* commandQueue;
-	uint32_t commandQueueDepth = 20;
-	StorageManagerIF *IPCStore;
 
+protected:
+	virtual ReturnValue_t isValidSubservice(uint8_t subservice);
+	/**
+	 * Once a TC Request is valid, the existence of the destination and interface is checked.
+	 * After that, its Message Queue ID is determined for the Commanding Service Base.
+	 * @param subservice
+	 * @param tcData Application Data of TC Packet
+	 * @param tcDataLen Application Data length
+	 * @param id MessageQueueId_t is stored here
+	 * @param objectId Target object ID
+	 * @return
+	 */
+	virtual ReturnValue_t getMessageQueueAndObject(uint8_t subservice,
+			const uint8_t *tcData, size_t tcDataLen, MessageQueueId_t *id,
+			object_id_t *objectId);
+
+	/**
+	 * After the Message Queue and Object ID are determined,
+	 * the command is prepared by using a FileSystemMessage which is sent to
+	 * the target memory handler.
+	 * @param message DeviceHandlerMessage
+	 * @param subservice
+	 * @param tcData
+	 * @param tcDataLen
+	 * @param state
+	 * @param objectId
+	 * @return
+	 */
+	virtual ReturnValue_t prepareCommand(CommandMessage* message,
+			uint8_t subservice, const uint8_t *tcData, size_t tcDataLen,
+			uint32_t *state, object_id_t objectId);
+
+	/**
+	 * Called by CSB template class and contains user specific implementation
+	 * of reply handling. Refer to CSB doc for more information
+	 * @param reply Command Message which contains information about the command
+	 * @param previousCommand
+	 * @param state
+	 * @param optionalNextCommand
+	 * @param objectId
+	 * @param isStep Flag value to mark steps of command execution
+	 * @return - @c RETURN_OK, @c EXECUTION_COMPLETE or @c NO_STEP_MESSAGE to generate TC verification success
+	 *         - @c INVALID_REPLY can handle unrequested replies
+	 *         - Anything else triggers a TC verification failure
+	 */
+	virtual ReturnValue_t handleReply(const CommandMessage *reply,
+			Command_t previousCommand, uint32_t *state,
+			CommandMessage *optionalNextCommand, object_id_t objectId,
+			bool *isStep);
+
+private:
+	ReturnValue_t checkAndAcquireTargetID(object_id_t* objectIdToSet, const uint8_t* tcData, uint32_t tcDataLen);
+	ReturnValue_t checkInterfaceAndAcquireMessageQueue(MessageQueueId_t* MessageQueueToSet, object_id_t* objectId);
+
+	ReturnValue_t prepareDeleteFileCommand(CommandMessage *message,
+					const uint8_t *tcData, uint32_t tcDataLen);
+
+	ReturnValue_t prepareCreateDirectoryCommand(CommandMessage *message,
+				const uint8_t *tcData, uint32_t tcDataLen);
+
+	ReturnValue_t prepareDeleteDirectoryCommand(CommandMessage *message,
+				const uint8_t *tcData, uint32_t tcDataLen);
+
+	ReturnValue_t prepareWriteCommand(CommandMessage *message,
+			const uint8_t *tcData, uint32_t tcDataLen);
+
+	ReturnValue_t prepareReadCommand(CommandMessage *message,
+			const uint8_t *tcData, uint32_t tcDataLen);
+
+
+	enum Subservice {
+		CREATE_FILE = 1, //!< [EXPORT] : [COMMAND] Create file
+		DELETE_FILE = 2, //!< [EXPORT] : [COMMADND] Delete file
+		CREATE_DIRECTORY = 9, //!<  [EXPORT] : [COMMAND] Create a directory
+		DELETE_DIRECTORY = 10, //!<  [EXPORT] : [COMMAND] Delete a directory
+		WRITE = 128, //!< [EXPORT] : [COMMAND] Write data to file
+		READ= 129, //!< [EXPORT] : [COMMAND] Read data from a file
+		READ_REPLY = 130 //!< [EXPORT] : [REPLY] Reply of subservice 129
+	};
 };
 
 
