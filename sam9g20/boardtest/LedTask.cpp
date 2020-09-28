85a378dcd8747930291ea4cafaa6665a28853dc4
Maximilian Luz <luzmaximilian@gmail.com>
2020-09-28 13:05:56 +0200

    Add missing includes

--- sam9g20/boardtest/LedTask.cpp
+++ sam9g20/boardtest/LedTask.cpp
@@ -5,6 +5,7 @@ extern "C" {
 }
 
 #include <cstdio>
+#include <cstdlib>
 
 LedTask::LedTask(object_id_t objectId, std::string name, LedModes ledMode):
         SystemObject(objectId), ledMode(ledMode) {
