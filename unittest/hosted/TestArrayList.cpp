cdca9aab6f7dc4b12522c87d1409565f440cc4e6
Robin.Mueller <robin.mueller.m@gmail.com>
2020-09-29 17:50:08 +0200

    array list test added

--- unittest/hosted/TestArrayList.cpp
+++ unittest/hosted/TestArrayList.cpp
@@ -7,10 +7,23 @@
  */
 TEST_CASE("Array List" , "[ArrayListTest]") {
 	//perform set-up here
-	ArrayList<uint16_t> list = ArrayList<uint16_t>(20);
+	ArrayList<uint16_t> list = ArrayList<uint16_t>(3);
+	list.insert(1);
+	list.insert(2);
+	ArrayList<uint16_t>::Iterator start = list.begin();
+	ArrayList<uint16_t>::Iterator end = list.end();
 	SECTION("SimpleTest") {
 		// set-up is run for each section
+		bool test = (start != end);
+		CHECK(test);
+		while(start != list.end()) {
+			start ++;
+		}
+		CHECK(start == end);
 
+		ReturnValue_t result = list.insert(3);
+		CHECK(result == retval::CATCH_OK);
+		CHECK(list.insert(6) == (uint16_t) ArrayList<uint16_t>::FULL);
 	}
 	// perform tear-down here
 }
