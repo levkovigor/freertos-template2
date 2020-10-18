a4bd5b4f5d2ac95ed9bedc0a14976bf63b5bab1c
Robin.Mueller <robin.mueller.m@gmail.com>
2020-10-19 00:43:28 +0200

    parameter srv working for sw img handler

--- sam9g20/core/ScrubbingEngine.h
+++ sam9g20/core/ScrubbingEngine.h
@@ -10,9 +10,10 @@ public:
     /**
      * The hamming code can be stored on FRAM or on the SD card.
      * For the AT91 board, the bootloader will always be stored on the SD
-     * card. For the iOBC, the bootloader
+     * card. For the iOBC, the bootloader can also be stored on the FRAM.
+     * This will be the  default setting.
      */
-    bool hammingCodeOnSdCard = true;
+    bool hammingCodeOnSdCard = false;
 private:
     SoftwareImageHandler* owner;
 
