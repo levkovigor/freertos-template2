9e51319507ec008ef017e7d2abfbcc0ab9883bbd
Robin Mueller <robin.mueller.m@gmail.com>
2020-11-29 13:01:51 +0100

    continued satellite system object

--- mission/SatelliteSystem.h
+++ mission/SatelliteSystem.h
@@ -1,7 +1,27 @@
 #ifndef MISSION_SATELLITESYSTEM_H_
 #define MISSION_SATELLITESYSTEM_H_
 
+#include <fsfw/subsystem/Subsystem.h>
 
+class SatelliteSystem: public Subsystem {
+public:
+    static constexpr uint8_t EVENT_MQ_DEPTH = 20;
+
+    SatelliteSystem(object_id_t setObjectId, object_id_t parent,
+            uint32_t maxNumberOfSequences, uint32_t maxNumberOfTables,
+            Mode_t noneMode, Mode_t bootMode, Mode_t safeMode, Mode_t idleMode);
+
+    ReturnValue_t initialize() override;
+private:
+    void checkEventQueue();
+
+    MessageQueueIF* eventQueue;
+
+    Mode_t noneMode = 0;
+    Mode_t bootMode = 0;
+    Mode_t safeMode = 0;
+    Mode_t idleMode = 0;
+};
 
 
 
