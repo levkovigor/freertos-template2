7f56fe44b67428fd23de5ddd821ca9b98aab590e
David <d.woodward@gmx.de>
2020-12-14 00:23:55 +0100

    added some dummy code for testing

--- mission/pus/Service11TelecommandScheduling.cpp
+++ mission/pus/Service11TelecommandScheduling.cpp
@@ -1,9 +1,23 @@
 #include "Service11TelecommandScheduling.h"
+#include "etl/utility.h"
 
 
 Service11TelecommandScheduling::Service11TelecommandScheduling(
         object_id_t objectId, uint16_t apid, uint8_t serviceId):
-        PusServiceBase(objectId, apid, serviceId) { }
+        PusServiceBase(objectId, apid, serviceId) { 
+
+
+        // just to test:
+        //--------------
+        TelecommandStruct dummyCommand;
+        dummyCommand.milliseconds = 100;
+        dummyCommand.storeId = 1;
+        uint32_t millisecKey = 100;
+
+        TelecommandMap.insert(etl::pair<uint32_t, TelecommandStruct>(millisecKey, dummyCommand));
+
+
+        }
 
 
 Service11TelecommandScheduling::~Service11TelecommandScheduling() { }
@@ -17,6 +31,6 @@ ReturnValue_t Service11TelecommandScheduling::handleRequest(
 
 
 ReturnValue_t Service11TelecommandScheduling::performService() {
-    
+
     return HasReturnvaluesIF::RETURN_OK;
 }
