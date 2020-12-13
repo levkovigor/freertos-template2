c457934d5e048a11a39936a9c409b26684b6ee27
David <d.woodward@gmx.de>
2020-12-14 00:09:39 +0100

    did some minor refactoring

--- mission/pus/Service11TelecommandScheduling.cpp
+++ mission/pus/Service11TelecommandScheduling.cpp
@@ -5,13 +5,16 @@ Service11TelecommandScheduling::Service11TelecommandScheduling(
         object_id_t objectId, uint16_t apid, uint8_t serviceId):
         PusServiceBase(objectId, apid, serviceId) { }
 
+
 Service11TelecommandScheduling::~Service11TelecommandScheduling() { }
+
 
 ReturnValue_t Service11TelecommandScheduling::handleRequest(
         uint8_t subservice) {
 
     return HasReturnvaluesIF::RETURN_OK;
 }
+
 
 ReturnValue_t Service11TelecommandScheduling::performService() {
     
