84b5a131cb7276c2c76481d7b8c0ce07fd15fbeb
Robin Mueller <robin.mueller.m@gmail.com>
2020-12-14 21:56:19 +0100

    version number corrected

--- fsfwconfig/FSFWConfig.h
+++ fsfwconfig/FSFWConfig.h
@@ -24,16 +24,14 @@
 //! If FSFW_OBJ_EVENT_TRANSLATION is set to one,
 //! additional output which requires the translation files translateObjects
 //! and translateEvents (and their compiled source files)
-#define FSFW_OBJ_EVENT_TRANSLATION	0
+#define FSFW_OBJ_EVENT_TRANSLATION	1
 
 #if FSFW_OBJ_EVENT_TRANSLATION == 1
-#define FSFW_DEBUG_OUTPUT 			1
 //! Specify whether info events are printed too.
 #define FSFW_DEBUG_INFO				1
 #include <translateObjects.h>
 #include <translateEvents.h>
 #else
-#define FSFW_DEBUG_OUTPUT			0
 #endif
 
 //! When using the newlib nano library, C99 support for stdio facilities
