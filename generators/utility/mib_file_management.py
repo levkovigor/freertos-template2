425bcd7f66b92c2b4a8b5d75d76b1aab94925a75
Robin Mueller <robin.mueller.m@gmail.com>
2021-01-02 20:13:20 +0100

    better print output

--- generators/utility/mib_file_management.py
+++ generators/utility/mib_file_management.py
@@ -4,7 +4,7 @@ import shutil
 import os
 
 
-def copy_file(filename: str, destination: str = ""):
+def copy_file(filename: str, destination: str = "", delete_existing_file: bool = False):
     if os.path.exists(filename):
         try:
             shutil.copy2(filename, destination)
@@ -12,7 +12,7 @@ def copy_file(filename: str, destination: str = ""):
             print("copy_file: File not found!")
             print(error)
         except shutil.SameFileError:
-            print("copy_file: File already exists!")
+            print("copy_file: Source and destination are the same!")
 
 
 def move_file(file_name: str, destination: str = ""):
