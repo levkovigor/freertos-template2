a5d21ab858d179acf3aabe655742809337af6e82
NguSa1 <sa.nguyen@gmx.de>
2021-01-15 22:40:45 +0100

    bootloader sdram small fix

--- bootloader/iobc/common/boot_iobc.c
+++ bootloader/iobc/common/boot_iobc.c
@@ -36,16 +36,16 @@ void perform_bootloader_core_operation() {
 int perform_iobc_copy_operation_to_sdram() {
     // determine which binary should be copied to SDRAM first.
 	BootSelect boot_select = BOOT_NOR_FLASH;
-	bool yes = false;
-	bool slot0 = false;
-	get_software_to_be_updated(&yes, &slot0);
-	int result = get_software_to_be_updated(&yes, &slot0);
+	bool enable = false;
+	VolumeId volume = false;
+	get_software_to_be_updated(&enable, &volume);
+	int result = get_software_to_be_updated(&enable, &volume);
 	if (result != 0) {
 		TRACE_ERROR("FRAM could not be read!\n\r");
 	}
 	else {
-		if (yes) {
-			if (slot0) {
+		if (enable) {
+			if (volume == SD_CARD_0) {
 				boot_select = BOOT_SD_CARD_0_UPDATE;
 			}
 			else {
