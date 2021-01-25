43a8aafab4571b9e3d70e4575b41a83dff33e349
Jan.Gerhards <jan@gerhards.net>
2021-01-25 09:51:22 +0100

    continue implementing EtlMapWrapper and add documentation

--- mission/memory/TmStoreFrontend.cpp
+++ mission/memory/TmStoreFrontend.cpp
@@ -6,6 +6,26 @@
  */
 
 
+//todo: remove code below, this is just used to test the EtlMapWrapper
+#include <mission/memory/tmstore/EtlMapWrapperBase.h>
+#include <mission/memory/tmstore/EtlMapWrapper.h>
+
+class TTT {
+	EtlMapWrapper<int, char, 3> *testBase;
+public:
+	TTT(EtlMapWrapper<int, char, 3> *test) {
+		testBase = test;
+	}
+	void test() {
+		testBase->clear();
+		testBase->insert(2, 'a');
+		testBase->get(2);
+		testBase->erase(2);
+		testBase->insert(2, 'a');
+		testBase->eraseByValue('a');
+	}
+};
+
 
 
 
--- mission/memory/tmstore/EtlMapWrapperBase.h
+++ mission/memory/tmstore/EtlMapWrapperBase.h
@@ -10,19 +10,21 @@
 #ifndef MISSION_MEMORY_TMSTORE_ETLMAPWRAPPERBASE_H_
 #define MISSION_MEMORY_TMSTORE_ETLMAPWRAPPERBASE_H_
 
+/*
+ * This class is the base class for a EtlMapWrapper. It enables using multiple instances of EtlMapWrapper
+ * with varying sizes in a common collection. To achieve this, create the Wrapper objects with their
+ * respective sizes. After that, create a pointer to them that points to an object of this base class.
+ * This can then be used without every pointer pointing to a slightly different class based on the sizes
+ * of the maps.
+ */
 template<typename TKey, typename TMapped>
-class EtlMapWrapperBase {
-	etl::map<int, int, 3> test;
+class EtlMapWrapperBase {  //todo: Constructor and Destructor needed?
 
 public:
-	virtual ReturnValue_t insert(TKey key, TMapped value) {
-		test.insert(std::pair<int, int>(1, 2));
-		test[5] = 7;
-		test.erase(1);
-	}
-
-	virtual ReturnValue_t erase(TMapped value) = 0;
-	virtual std::pair<ReturnValue_t, TMapped&> get(TKey key) = 0;
+	virtual ReturnValue_t insert(TKey key, TMapped value) = 0;
+	virtual ReturnValue_t erase(TKey key) = 0;
+	virtual int eraseByValue(TMapped value) = 0;
+	virtual std::pair<ReturnValue_t, TMapped*> get(TKey key) = 0;
 	virtual void clear() = 0;
 };
 
