f669bf87028e35c2623db76469b6d544983ec520
David <d.woodward@gmx.de>
2021-01-29 20:14:31 +0100

    moved handleReq into private subfunction

--- mission/pus/Service11TelecommandScheduling.cpp
+++ mission/pus/Service11TelecommandScheduling.cpp
@@ -19,57 +19,8 @@ Service11TelecommandScheduling::~Service11TelecommandScheduling() { }
 ReturnValue_t Service11TelecommandScheduling::handleRequest(uint8_t subservice) {
 
     if (subservice == Subservice::INSERT_ACTIVITY){
-
-        // storage for the raw data to be received
-        const uint8_t* pRawData = nullptr;  // "(non-const) pointer to const unsigned 8-bit int"
-
-        // get serialized data packet
-        pRawData = this->currentPacket.getApplicationData();
-//        if (ret != RETURN_OK){
-//            // data not retrieved successfully
-//            return ret;
-//        }
-        if (pRawData == nullptr){
-            //NOTE: Don't know whether this is necessary, prevents nullpointer crashes though
-            return RETURN_FAILED;
-        }
-
-        size_t appDataSize = this->currentPacket.getApplicationDataSize();
-
-        sif::printInfo("currentPacket size: %d\n", this->currentPacket.getApplicationDataSize());
-
-
-        //test
-        uint32_t deserializedTimestamp = 0;
-
-        // for better understanding:
-        // pRawData: raw data buffer to be de-serialized
-        // size: remaining size of buffer to de-serialize. Is decreased by function (until 0 I assume)
-        // I assume: Function de-serializes byte-wise until remaining size is 0
-        ReturnValue_t ret = SerializeAdapter::deSerialize<uint32_t>(&deserializedTimestamp,
-                &pRawData, &appDataSize, SerializeIF::Endianness::BIG);
-        if (ret != RETURN_OK){
-            return ret;
-        }
-
-        // deserializedTimestamp should now be the correct UNIX timestamp in sec,
-        // without any further bit-shifting etc.
-
-
-        // get store address
-        store_address_t addr = this->currentPacket.getStoreAddress();   // this can be done nicer
-        if (addr.raw == storeId::INVALID_STORE_ADDRESS){
-            return HasReturnvaluesIF::RETURN_FAILED;
-        }
-
-        TelecommandStruct tc(deserializedTimestamp, addr);
-        auto it = telecommandMap.insert(std::pair<uint32_t, TelecommandStruct>(deserializedTimestamp, tc));
-        if (it == telecommandMap.end()){
-            return HasReturnvaluesIF::RETURN_FAILED;
-        }
-
+        return this->handleRequest_InsertActivity();
     }
-
 
 
     return HasReturnvaluesIF::RETURN_OK;
@@ -101,4 +52,54 @@ ReturnValue_t Service11TelecommandScheduling::initialize() {
     }
 
     return res;
+}
+
+
+
+ReturnValue_t Service11TelecommandScheduling::handleRequest_InsertActivity() {
+
+    // storage for the raw data to be received
+    const uint8_t* pRawData = nullptr;  // "(non-const) pointer to const unsigned 8-bit int"
+
+    // get serialized data packet
+    pRawData = this->currentPacket.getApplicationData();
+    if (pRawData == nullptr) {
+        return RETURN_FAILED;
+    }
+
+    size_t appDataSize = this->currentPacket.getApplicationDataSize();
+
+    //DEBUG
+    sif::printInfo("currentPacket size: %d\n", this->currentPacket.getApplicationDataSize());
+
+
+    uint32_t deserializedTimestamp = 0;
+
+    // for better understanding:
+    // pRawData: raw data buffer to be de-serialized
+    // size: remaining size of buffer to de-serialize. Is decreased by function (until 0 I assume)
+    // I assume: Function de-serializes byte-wise until remaining size is 0
+    ReturnValue_t ret = SerializeAdapter::deSerialize<uint32_t>(&deserializedTimestamp,
+            &pRawData, &appDataSize, SerializeIF::Endianness::BIG);
+    if (ret != RETURN_OK){
+        return ret;
+    }
+
+    // deserializedTimestamp should now be the correct UNIX timestamp in sec,
+    // without any further bit-shifting etc.
+
+
+    // get store address
+    store_address_t addr = this->currentPacket.getStoreAddress();   // this can be done nicer
+    if (addr.raw == storeId::INVALID_STORE_ADDRESS) {
+        return HasReturnvaluesIF::RETURN_FAILED;
+    }
+
+    TelecommandStruct tc(deserializedTimestamp, addr);
+    auto it = telecommandMap.insert(std::pair<uint32_t, TelecommandStruct>(deserializedTimestamp, tc));
+    if (it == telecommandMap.end()){
+        return HasReturnvaluesIF::RETURN_FAILED;
+    }
+
+    return HasReturnvaluesIF::RETURN_OK;
 }
--- mission/pus/Service11TelecommandScheduling.h
+++ mission/pus/Service11TelecommandScheduling.h
@@ -9,27 +9,28 @@
 
 class Service11TelecommandScheduling final: public PusServiceBase {
 public:
-    Service11TelecommandScheduling(object_id_t objectId, uint16_t apid,
-            uint8_t serviceId);
+
+    enum Subservice {
+                ENABLE_SCHEDULING = 1,
+                DISABLE_SCHEDULING = 2,
+                RESET_SCHEDULING = 3,
+                INSERT_ACTIVITY = 4,    // basic
+                DELETE_ACTIVITY = 5,    // basic
+                FILTER_ACTIVITY = 6,
+                TIMESHIFT_ACTIVITY = 7,
+                DETAIL_REPORT = 9,
+                TIMEBASE_SCHEDULE_DETAIL_REPORT = 10,
+                TIMESHIFT_ALL_SCHEDULE_ACTIVITIES = 15
+            };
+
+
+    Service11TelecommandScheduling(object_id_t objectId, uint16_t apid, uint8_t serviceId);
     ~Service11TelecommandScheduling();
 
     /** PusServiceBase overrides */ 
     ReturnValue_t handleRequest(uint8_t subservice) override;
     ReturnValue_t performService() override;
     ReturnValue_t initialize() override;
-
-    enum Subservice {
-        ENABLE_SCHEDULING = 1,
-        DISABLE_SCHEDULING = 2,
-        RESET_SCHEDULING = 3,
-        INSERT_ACTIVITY = 4,    // basic
-        DELETE_ACTIVITY = 5,    // basic
-        FILTER_ACTIVITY = 6,
-        TIMESHIFT_ACTIVITY = 7,
-        DETAIL_REPORT = 9,
-        TIMEBASE_SCHEDULE_DETAIL_REPORT = 10,
-        TIMESHIFT_ALL_SCHEDULE_ACTIVITIES = 15
-    };
 
 
 private:
@@ -52,13 +53,13 @@ private:
      * The telecommand map uses the exectution time as a Unix time stamp as
      * the key. This is mapped to a generic telecommand struct.
      */
-
-    //NOTE: Is "UNIX timestamp = seconds since 1970, returned by std::time() for example?"
-
     using TelecommandMap = etl::multimap<uint32_t, TelecommandStruct,
             config::MAX_STORED_TELECOMMANDS>;
 
     TelecommandMap telecommandMap;
+
+
+    ReturnValue_t handleRequest_InsertActivity();
 
 };
 
