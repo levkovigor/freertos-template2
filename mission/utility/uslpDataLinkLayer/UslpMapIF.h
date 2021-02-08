a61af50b912eb471182f70c8ff600bb20c1086b6
Luc Rajer <luc.rajer@gmx.de>
2021-02-08 10:26:48 +0100

    UslpMapIF added

---
+++ mission/utility/uslpDataLinkLayer/UslpMapIF.h
@@ -0,0 +1,54 @@
+#ifndef MISSION_UTILITY_USLPDATALINKLAYER_USLPMAPIF_H_
+#define MISSION_UTILITY_USLPDATALINKLAYER_USLPMAPIF_H_
+#include <fsfw/datalinklayer/CCSDSReturnValuesIF.h>
+#include "USLPTransferFrame.h"
+/**
+ * @brief       Interface for a USLP Multiplexer Access Point
+ * @details     Functions for packing and unpacking frames
+ *
+ * @author      L. Rajer
+ */
+class UslpMapIF : public CCSDSReturnValuesIF {
+protected:
+
+public:
+    /**
+     * Empty virtual destructor.
+     */
+    virtual ~UslpMapIF() {
+    }
+    /**
+     * @brief Method to call to handle a single Transfer Frame.
+     * @details The method tries to extract Packets from the frame as stated in the Standard and
+     *          forwards them to their destination
+     * @param frame
+     * @return
+     */
+    virtual ReturnValue_t extractPackets( USLPTransferFrame* frame ) = 0;
+
+    /**
+     * @brief Packs a transfer frame with the Multiplexer Access Point Access Service
+     * @details This means privately formated SDUs
+     * @return
+     */
+    // TODO: Best parameter for this
+    virtual ReturnValue_t packFrameMapa();
+
+    /**
+      * @brief Packs a transfer frame with the Multiplexer Access Point Packket Service
+      * @details This means space packets
+      * @return
+      */
+    // TODO: Best parameter for this
+    virtual ReturnValue_t packFrameMapp();
+    /**
+     * Any post-instantiation initialization shall be done in this method.
+     * @return
+     */
+    virtual ReturnValue_t initialize() = 0;
+};
+
+
+
+
+#endif /* MISSION_UTILITY_USLPDATALINKLAYER_USLPMAPIF_H_ */
