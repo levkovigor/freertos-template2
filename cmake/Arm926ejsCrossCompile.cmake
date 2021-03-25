459f4e410ef41d060ecb80e138adb30d533343e5
Robin Mueller <robin.mueller.m@gmail.com>
2021-03-25 14:35:17 +0100

    freertos is tricky

--- cmake/Arm926ejsCrossCompile.cmake
+++ cmake/Arm926ejsCrossCompile.cmake
@@ -19,14 +19,40 @@ set(CROSS_COMPILE_CXX "${CROSS_COMPILE}-g++")
 set(CROSS_COMPILE_OBJCOPY "${CROSS_COMPILE}-objcopy")
 set(CROSS_COMPILE_SIZE "${CROSS_COMPILE}-size")
 
-# At the very least, cross compile gcc and g++ have to be set!
+# At the very least, cross compile gcc, g++ and objcopy have to be set!
 find_program (CROSS_COMPILE_CC_FOUND ${CROSS_COMPILE_CC} REQUIRED)
 find_program (CROSS_COMPILE_CXX_FOUND ${CROSS_COMPILE_CXX} REQUIRED)
+find_program (CROSS_COMPILE_OBJCOPY_FOUND ${CROSS_COMPILE_OBJCOPY} REQUIRED)
+
+# Size utility optional.
+find_program (CMAKE_SIZE ${CROSS_COMPILE_SIZE})
 
 set(CMAKE_CROSSCOMPILING TRUE)
+
+# Define name of the target system
+set(CMAKE_SYSTEM_NAME "Generic")
+set(CMAKE_SYSTEM_PROCESSOR "arm")
 
 # Define the compiler
 set(CMAKE_C_COMPILER ${CROSS_COMPILE_CC})
 set(CMAKE_CXX_COMPILER ${CROSS_COMPILE_CXX})
+set(CMAKE_ASM_FLAGS "-x assembler-with-cpp")
+
+set(ABI_FLAGS
+    -mcpu=arm926ej-s
+    --specs=nosys.specs 
+    --specs=nano.specs
+)
+
+string (REPLACE ";" " " ABI_FLAGS "${ABI_FLAGS}")
+
+set(CMAKE_C_FLAGS 
+    ${ABI_FLAGS}
+    CACHE STRING "C flags for the ARM926EJ-S processor with FreeRTOS"
+)
+set(CMAKE_CXX_FLAGS 
+    "${CMAKE_C_FLAGS}" 
+    CACHE STRING "C++ flags for the ARM926EJ-S processor with FreeRTOS"
+)
 
 # No system root used for bare metal compilation
