45101f1c0eca1ff1f1232100435a5135a1e872f8
Robin Mueller <robin.mueller.m@gmail.com>
2021-03-25 11:24:32 +0100

    c++ compatibility

--- sam9g20/common/lowlevel.h
+++ sam9g20/common/lowlevel.h
@@ -1,11 +1,20 @@
 #ifndef SAM9G20_UTILITY_LOWLEVEL_H_
 #define SAM9G20_UTILITY_LOWLEVEL_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
 unsigned int asm_get_cpsr(void);
 void asm_set_cpsr(unsigned int val);
 void print_processor_state(void);
 
 void asm_disable_irq();
 void asm_enable_irq();
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* SAM9G20_UTILITY_LOWLEVEL_H_ */
