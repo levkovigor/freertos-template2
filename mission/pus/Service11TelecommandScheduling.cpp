9e495cfe014c1d904de6d14bf6838f173e731845
David <d.woodward@gmx.de>
2021-03-26 23:30:48 +0100

    refactored to switch-case

--- mission/pus/Service11TelecommandScheduling.cpp
+++ mission/pus/Service11TelecommandScheduling.cpp
@@ -12,9 +12,7 @@
 
 Service11TelecommandScheduling::Service11TelecommandScheduling(
         object_id_t objectId, uint16_t apid, uint8_t serviceId):
-        PusServiceBase(objectId, apid, serviceId) {
-
-}
+        PusServiceBase(objectId, apid, serviceId) { }
 
 
 Service11TelecommandScheduling::~Service11TelecommandScheduling() { }
