f8b40348256ee576bdf9a32f31df927537cc1b0c
Robin Mueller <robin.mueller.m@gmail.com>
2021-03-31 00:25:11 +0200

    unified versioning, common object list stub

--- common/OBSWVersion.h
+++ common/OBSWVersion.h
@@ -1,10 +1,10 @@
 #ifndef FSFWCONFIG_OBSWVERSION_H_
 #define FSFWCONFIG_OBSWVERSION_H_
 
-const char* const SW_NAME = "sputnik";
+const char* const SW_NAME = "explorer";
 
 #define SW_VERSION          3
 #define SW_SUBVERSION       0
-#define SW_SUBSUBVERSION    0
+#define SW_SUBSUBVERSION    1
 
 #endif /* FSFWCONFIG_OBSWVERSION_H_ */
