5098eec145f81d9ca0634a293f8cec279611fa13
Robin Mueller Flatsat <robin.mueller.m@gmail.com>
2021-04-21 14:19:14 +0200

    configured for send test again

--- bsp_sam9g20/boardtest/UART2TestTask.cpp
+++ bsp_sam9g20/boardtest/UART2TestTask.cpp
@@ -21,8 +21,9 @@ extern "C" {
  * If you send \"12ab\", you will receive back \"12AB\" on the same bus.
  */
 
-UART2TestTask::UART2TestTask(const char * printName, object_id_t objectId) :
-                    SystemObject(objectId), printName(printName), uartMode(NON_BLOCKING), uartState(WRITE) {
+UART2TestTask::UART2TestTask(const char * printName, object_id_t objectId):
+        SystemObject(objectId), printName(printName), uartMode(SEND_TEST),
+        uartState(WRITE) {
     configBus2.mode = AT91C_US_USMODE_NORMAL | AT91C_US_CLKS_CLOCK |
             AT91C_US_CHRL_8_BITS | AT91C_US_PAR_NONE | AT91C_US_OVER_16 |
             AT91C_US_NBSTOP_1_BIT;
