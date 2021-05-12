2d7576d2d4628c9c7586817b26aa1a3cb14dc265
Robin Mueller <robin.mueller.m@gmail.com>
2021-05-12 14:09:55 +0200

    wrong include fixed

--- bsp_hosted/fsfwconfig/objects/translateObjects.h
+++ bsp_hosted/fsfwconfig/objects/translateObjects.h
@@ -1,14 +1,7 @@
-/*
- * translateObjects.h
- *
- *  Created on: 28 May 2019
- *      Author: Robin
- */
-
 #ifndef CONFIG_OBJECTS_TRANSLATEOBJECTS_H_
 #define CONFIG_OBJECTS_TRANSLATEOBJECTS_H_
 
-#include <fsfw/objectmanager/ObjectManagerIF.h>
+#include <fsfw/objectmanager/SystemObjectIF.h>
 
 const char* translateObject(object_id_t object);
 
