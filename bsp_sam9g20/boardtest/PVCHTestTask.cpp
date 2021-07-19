eb286db831594899642035543b130f382b477bce
Robin Mueller <robin.mueller.m@gmail.com>
2021-07-19 22:43:32 +0200

    updated project file

--- bsp_sam9g20/boardtest/PVCHTestTask.cpp
+++ bsp_sam9g20/boardtest/PVCHTestTask.cpp
@@ -35,6 +35,25 @@ ReturnValue_t PVCHTestTask::initialize() {
     txBuf[0] = PCA9554Regs::CONFIG;
     txBuf[1] = 0x00;
     result = i2cComIF->sendMessage(i2cCookie, txBuf.data(), 2);
+    if(result != HasReturnvaluesIF::RETURN_OK) {
+        sif::printWarning("PVCHTestTask::initialize: Configuring ports as output failed!\n");
+    }
+
+    // Read back registers to check whether it was set correctly
+    result = i2cComIF->requestReceiveMessage(i2cCookie, 1);
+    i2cCookie->getSemaphoreObjectHandle();
+    txBuf[0] = PCA9554Regs::OUTPUT_PORT;
+    txBuf[1] = 0xff;
+    result = i2cComIF->sendMessage(i2cCookie, txBuf.data(), 2);
+    if(result != HasReturnvaluesIF::RETURN_OK) {
+        sif::printWarning("PVCHTestTask::initialize: Setting all outputs high failed!\n");
+    }
+
+    // Read back
+    i2cComIF->requestReceiveMessage(i2cCookie, 1);
+    if(result != HasReturnvaluesIF::RETURN_OK) {
+        sif::printWarning("PVCHTestTask::initialize: Reading output failed!\n");
+    }
     return HasReturnvaluesIF::RETURN_OK;
 }
 
