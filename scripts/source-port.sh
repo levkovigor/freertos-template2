3c4f48abbb90f4e6c4cc8fbcf0bc639a1044b004
Robin Mueller <robin.mueller.m@gmail.com>
2021-08-05 14:48:27 +0200

    this is working

--- scripts/source-port.sh
+++ scripts/source-port.sh
@@ -1,4 +1,4 @@
 echo "Setting up SOURCE Flatsat Tunnel"
 ssh -L 2336:localhost:2331 \
 	source@flatsat.source.absatvirt.lw \
-	-t "CONSOLE_PREFIX=[SOURCE Port] /bin/bash"
+	-t 'CONSOLE_PREFIX="[SOURCE Port]" /bin/bash'
