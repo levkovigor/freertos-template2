47cbd981037233b23403a72e328de84e775fab4c
Robin Mueller <robin.mueller.m@gmail.com>
2021-09-07 17:58:41 +0200

    added ring buffer analyzer

--- bsp_sam9g20/core/RingBufferAnalyzer.cpp
+++ bsp_sam9g20/core/RingBufferAnalyzer.cpp
@@ -2,84 +2,117 @@
 
 #include <fsfw/serviceinterface/ServiceInterface.h>
 #include <fsfw/globalfunctions/DleEncoder.h>
-#include <fsfw/ipc/MutexGuard.h>
-#include <OBSWConfig.h>
+#include <fsfw/ipc/MutexHelper.h>
+#include <fsfwconfig/OBSWConfig.h>
 
 #include <cstring>
+#include <sam9g20/comIF/RS485DeviceComIF.h>
+#include "../../mission/utility/uslpDataLinkLayer/USLPTransferFrame.h"
 
-RingBufferAnalyzer::RingBufferAnalyzer(SharedRingBuffer *ringBuffer,
-        AnalyzerModes mode):
+RingBufferAnalyzer::RingBufferAnalyzer(SharedRingBuffer *ringBuffer, AnalyzerModes mode) :
         mode(mode), ringBuffer(ringBuffer) {
-    if(ringBuffer == nullptr) {
+    if (ringBuffer == nullptr) {
 #if FSFW_CPP_OSTREAM_ENABLED == 1
         sif::error << "SerialAnalyzerTask::SerialAnalyzerTask: "
                 "Passed ring buffer invalid!" << std::endl;
-#else
-        sif::printError("SerialAnalyzerTask::SerialAnalyzerTask: "
-                "Passed ring buffer invalid!\n");
 #endif
-        return;
+    } else if (mode != AnalyzerModes::DLE_ENCODING) {
+#if FSFW_CPP_OSTREAM_ENABLED == 1
+        sif::error << "SerialAnalyzerTask::SerialAnalyzerTask: "
+                "Wrong constructor for DLE mode!" << std::endl;
+#endif
     }
     analysisVector = std::vector<uint8_t>(ringBuffer->getMaxSize());
 
 }
 
-ReturnValue_t RingBufferAnalyzer::checkForPackets(uint8_t* receptionBuffer,
-        size_t maxData, size_t* packetSize) {
-    if(receptionBuffer == nullptr or packetSize == nullptr) {
+RingBufferAnalyzer::RingBufferAnalyzer(SharedRingBuffer *ringBuffer,
+        std::map<uint8_t, size_t> *virtualChannelFrameSizes, AnalyzerModes mode) :
+        mode(mode), ringBuffer(ringBuffer), virtualChannelFrameSizes(virtualChannelFrameSizes) {
+
+    if (ringBuffer == nullptr) {
+#if FSFW_CPP_OSTREAM_ENABLED == 1
+        sif::error << "SerialAnalyzerTask::SerialAnalyzerTask: "
+                "Passed ring buffer invalid!" << std::endl;
+#endif
+    } else if (mode != AnalyzerModes::USLP_FRAMES) {
+#if FSFW_CPP_OSTREAM_ENABLED == 1
+        sif::error << "SerialAnalyzerTask::SerialAnalyzerTask: "
+                "Wrong constructor for USLP mode!" << std::endl;
+#endif
+    } else if (virtualChannelFrameSizes == nullptr || virtualChannelFrameSizes->empty()) {
+#if FSFW_CPP_OSTREAM_ENABLED == 1
+        sif::error << "SerialAnalyzerTask::SerialAnalyzerTask: "
+                "No VCID lengths provided!" << std::endl;
+#endif
+    }
+
+    analysisVector = std::vector<uint8_t>(ringBuffer->getMaxSize());
+
+    // The first 20 bits of the USLP header are constant and used instead of a sync marker
+    uslpHeaderMarker[0] = (config::RS485_USLP_TFVN << 4) + (config::RS485_USLP_SCID >> 12);
+    uslpHeaderMarker[1] = (config::RS485_USLP_SCID & 0x0FF0) >> 4;
+    uslpHeaderMarker[2] = (config::RS485_USLP_SCID & 0x000f) << 4;
+
+}
+
+ReturnValue_t RingBufferAnalyzer::checkForPackets(uint8_t *receptionBuffer, size_t maxData,
+        size_t *packetSize) {
+    if (receptionBuffer == nullptr or packetSize == nullptr) {
         return HasReturnvaluesIF::RETURN_FAILED;
     }
 
     ReturnValue_t result = readRingBuffer();
-    if(result == NO_PACKET_FOUND) {
+    if (result == NO_PACKET_FOUND) {
         return result;
-    }
-    else if(result != HasReturnvaluesIF::RETURN_OK) {
+    } else if (result != HasReturnvaluesIF::RETURN_OK) {
         return result;
     }
 
-    if(mode == AnalyzerModes::DLE_ENCODING) {
-        result = handleDleParsing(receptionBuffer, maxData, packetSize);
-    }
+    result = handleParsing(receptionBuffer, maxData, packetSize);
+
     return result;
 }
 
 ReturnValue_t RingBufferAnalyzer::readRingBuffer() {
     ReturnValue_t result = HasReturnvaluesIF::RETURN_OK;
-    MutexGuard lock(ringBuffer->getMutexHandle(),
-            MutexIF::TimeoutType::WAITING, config::RS232_MUTEX_TIMEOUT);
+    MutexHelper lock(ringBuffer->getMutexHandle(), MutexIF::TimeoutType::WAITING,
+            config::RS232_MUTEX_TIMEOUT);
     size_t dataToRead = ringBuffer->getAvailableReadData();
-    if(dataToRead == 0) {
+    if (dataToRead == 0) {
         return NO_PACKET_FOUND;
     }
 
-    if(dataToRead > currentBytesRead) {
+    if (dataToRead > currentBytesRead) {
         currentBytesRead = dataToRead;
-        result = ringBuffer->readData(analysisVector.data(),
-                currentBytesRead);
-        if(result != HasReturnvaluesIF::RETURN_OK) {
+        result = ringBuffer->readData(analysisVector.data(), currentBytesRead);
+        if (result != HasReturnvaluesIF::RETURN_OK) {
             return result;
         }
-    }
-    else {
+    } else {
         // no new data.
         return NO_PACKET_FOUND;
     }
     return result;
 }
 
-ReturnValue_t RingBufferAnalyzer::handleDleParsing(uint8_t* receptionBuffer,
-        size_t maxSize, size_t* packetSize) {
+ReturnValue_t RingBufferAnalyzer::handleParsing(uint8_t *receptionBuffer, size_t maxSize,
+        size_t *packetSize) {
     size_t readSize = 0;
-    ReturnValue_t result = parseForDleEncodedPackets(currentBytesRead,
-            receptionBuffer, maxSize, packetSize, &readSize);
-    if(result == HasReturnvaluesIF::RETURN_OK) {
+    ReturnValue_t result = NO_PACKET_FOUND;
+    if (mode == AnalyzerModes::DLE_ENCODING) {
+        result = parseForDleEncodedPackets(currentBytesRead, receptionBuffer, maxSize, packetSize,
+                &readSize);
+    } else if (mode == AnalyzerModes::USLP_FRAMES) {
+        result = parseForUslpFrames(currentBytesRead, receptionBuffer, maxSize, packetSize,
+                &readSize);
+    }
+    if (result == HasReturnvaluesIF::RETURN_OK) {
         // Packet found, advance read pointer.
         currentBytesRead = 0;
         ringBuffer->deleteData(readSize);
-    }
-    else if(result == POSSIBLE_PACKET_LOSS) {
-        // STX or ETX found at wrong place
+    } else if (result == POSSIBLE_PACKET_LOSS) {
+        // Markers found at wrong place
         // which might be a hint for a possibly lost packet.
         currentBytesRead = 0;
         ringBuffer->deleteData(readSize);
@@ -88,19 +121,17 @@ ReturnValue_t RingBufferAnalyzer::handleDleParsing(uint8_t* receptionBuffer,
     return result;
 }
 
-ReturnValue_t RingBufferAnalyzer::parseForDleEncodedPackets(
-        size_t bytesToRead, uint8_t* receptionBuffer, size_t maxSize,
-        size_t* packetSize, size_t* readSize) {
+ReturnValue_t RingBufferAnalyzer::parseForDleEncodedPackets(size_t bytesToRead,
+        uint8_t *receptionBuffer, size_t maxSize, size_t *packetSize, size_t *readSize) {
     bool stxFound = false;
     size_t stxIdx = 0;
-    for(size_t vectorIdx = 0; vectorIdx < bytesToRead; vectorIdx ++) {
+    for (size_t vectorIdx = 0; vectorIdx < bytesToRead; vectorIdx++) {
         // handle STX char
-        if(analysisVector[vectorIdx] == DleEncoder::STX_CHAR) {
-            if(not stxFound) {
+        if (analysisVector[vectorIdx] == DleEncoder::STX_CHAR) {
+            if (not stxFound) {
                 stxFound = true;
                 stxIdx = vectorIdx;
-            }
-            else {
+            } else {
                 // might be lost packet, so we should advance the read pointer
                 // without skipping the STX
                 *readSize = vectorIdx;
@@ -108,29 +139,75 @@ ReturnValue_t RingBufferAnalyzer::parseForDleEncodedPackets(
             }
         }
         // handle ETX char
-        if(analysisVector[vectorIdx] == DleEncoder::ETX_CHAR) {
-            if(stxFound) {
-                // This is propably a packet, so we decode it.
+        if (analysisVector[vectorIdx] == DleEncoder::ETX_CHAR) {
+            if (stxFound) {
+                // This is probably a packet, so we decode it.
 
-                ReturnValue_t result = DleEncoder::decode(
-                        &analysisVector[stxIdx],
-                        bytesToRead - stxIdx, readSize,
-                        receptionBuffer, maxSize, packetSize);
-                if(result == HasReturnvaluesIF::RETURN_OK) {
+                ReturnValue_t result = DleEncoder::decode(&analysisVector[stxIdx],
+                        bytesToRead - stxIdx, readSize, receptionBuffer, maxSize, packetSize);
+                if (result == HasReturnvaluesIF::RETURN_OK) {
                     return HasReturnvaluesIF::RETURN_OK;
-                }
-                else {
+                } else {
                     // invalid packet, skip.
                     *readSize = ++vectorIdx;
                     return POSSIBLE_PACKET_LOSS;
                 }
-            }
-            else {
+            } else {
                 // might be lost packet, so we should advance the read pointer
                 *readSize = ++vectorIdx;
                 return POSSIBLE_PACKET_LOSS;
             }
         }
+    }
+    return NO_PACKET_FOUND;
+}
+
+ReturnValue_t RingBufferAnalyzer::parseForUslpFrames(size_t bytesToRead, uint8_t *receptionBuffer,
+        size_t maxSize, size_t *packetSize, size_t *readSize) {
+    std::map<uint8_t, size_t>::iterator iter = virtualChannelFrameSizes->end();
+
+    /* Prevents  a subtraction from size_t that results in a negative value (bad)   */
+    if(bytesToRead < config::RS485_MIN_SERIAL_FRAME_SIZE){
+        return NO_PACKET_FOUND;
+    }
+    // This only works for byte aligned data, but this is the case for RS485
+    for (size_t vectorIdx = 0; vectorIdx <= bytesToRead - config::RS485_MIN_SERIAL_FRAME_SIZE;
+            vectorIdx++) {
+
+        // Check first 20 bits of packet for frame
+        if (analysisVector[vectorIdx] == uslpHeaderMarker[0]
+                && analysisVector[vectorIdx + 1] == uslpHeaderMarker[1]
+                && (analysisVector[vectorIdx + 2] & 0xF0) == uslpHeaderMarker[2]) {
+            // Get VCID
+            // We don't need to analyze the whole frame here, so we just get the relevant bits
+            uint8_t vcid = ((analysisVector[vectorIdx + 2] & 0b00000111) << 3)
+                    + ((analysisVector[vectorIdx + 3] & 0b11100000) >> 5);
+
+            iter = virtualChannelFrameSizes->find(vcid);
+
+            if (iter == virtualChannelFrameSizes->end()) {
+                // If we don't know the VCID, we either read at the wrong place, or we received an
+                // unknown VCID, both are not good, so we just skip this section by advancing the
+                // read pointer
+                *readSize = ++vectorIdx;
+                return POSSIBLE_PACKET_LOSS;
+            } else {
+                // Check if the buffer is larger enough for the packet
+                if (iter->second > maxSize) {
+                    return HasReturnvaluesIF::RETURN_FAILED;
+                } else {
+                    *packetSize = iter->second;
+                    // If we would decode the USLP Frame here this would be more relevant, here it
+                    // is just to be compatible with the DLE encoding part
+                    *readSize = iter->second;
+                    (void) std::memcpy(receptionBuffer, &analysisVector[vectorIdx], *packetSize);
+                    return HasReturnvaluesIF::RETURN_OK;
+                }
+
+            }
+
+        }
+
     }
     return NO_PACKET_FOUND;
 }
--- bsp_sam9g20/core/RingBufferAnalyzer.h
+++ bsp_sam9g20/core/RingBufferAnalyzer.h
@@ -3,61 +3,81 @@
 
 #include <fsfw/container/SharedRingBuffer.h>
 #include <vector>
+#include <map>
 
 enum class AnalyzerModes {
-	DLE_ENCODING //!< DLE encoded packets.
+    DLE_ENCODING, //!< DLE encoded packets.
+    USLP_FRAMES //!< USLP frames with TRUNCATED primary header
 };
 
 /**
- * @brief 	Analyzer task which checks a supplied ring buffer for
- * 			encoded packets.
+ * @brief   Analyzer task which checks a supplied ring buffer for
+ *          encoded packets.
  * @details
  * @author  R. Mueller
  */
 class RingBufferAnalyzer {
 public:
-	static constexpr uint8_t INTERFACE_ID = CLASS_ID::SERIAL_ANALYZER;
-	static constexpr ReturnValue_t NO_PACKET_FOUND = MAKE_RETURN_CODE(1);
-	static constexpr ReturnValue_t POSSIBLE_PACKET_LOSS = HasReturnvaluesIF::
-	        makeReturnCode(INTERFACE_ID, 2);
+    static constexpr uint8_t INTERFACE_ID = CLASS_ID::SERIAL_ANALYZER;
+    static constexpr ReturnValue_t NO_PACKET_FOUND = MAKE_RETURN_CODE(1);
+    static constexpr ReturnValue_t POSSIBLE_PACKET_LOSS = HasReturnvaluesIF::
+            makeReturnCode(INTERFACE_ID, 2);
 
-	/**
-	 * Initialize the serial analyzer with a supplied shared ring buffer.
-	 * @param buffer
-	 * @param mode
-	 */
-	RingBufferAnalyzer(SharedRingBuffer* buffer,
-			AnalyzerModes mode = AnalyzerModes::DLE_ENCODING);
+    /**
+     * Initialize the serial analyzer for DLE encoding with a supplied shared ring buffer.
+     * @param buffer
+     * @param mode
+     */
+    RingBufferAnalyzer(SharedRingBuffer* buffer,
+            AnalyzerModes mode = AnalyzerModes::DLE_ENCODING);
 
-	/**
-	 * Search for DLE encoded packets
-	 * @param buffer If a packet is found, it will be copied into that buffer
-	 * @param maxSize Maximum size of the supplied buffer. Should be large
-	 * enough to accomodate maximum designated packet size
-	 * @param packetSize Found packet size
-	 * @return
-	 * -@c RETURN_OK if a packet was found. Read pointer is incremented.
-	 * -@c NO_PACKET_FOUND if no packet was found.
-	 * -@c POSSIBLE_LOST_PACKET if there is a possiblity of a lost packet
-	 *     (end marker found without start marker). Read pointer is incremented.
-	 */
-	ReturnValue_t checkForPackets(uint8_t* buffer,
-			size_t maxData, size_t* packetSize);
+    /**
+     * Initialize the serial analyzer for fixed size USLP frames a supplied shared ring buffer.
+     * @param buffer
+     * @param virtualChannelFrameSizes Pointer to map with frame sizes for each VC
+     * @param mode
+     */
+    // TODO: Possibly Reference
+    RingBufferAnalyzer(SharedRingBuffer* buffer, std::map<uint8_t, size_t>* virtualChannelFrameSizes,
+            AnalyzerModes mode = AnalyzerModes::USLP_FRAMES);
+
+    /**
+     * Search for DLE encoded packets or USLP Frames
+     * @param buffer If a packet/frame is found, it will be copied into that buffer
+     * @param maxSize Maximum size of the supplied buffer. Should be large
+     * enough to accomodate maximum designated packet/frame size
+     * @param packetSize Found packet/frame size
+     * @return
+     * -@c RETURN_OK if a packet/frame was found. Read pointer is incremented.
+     * -@c NO_PACKET_FOUND if no packet/frame was found.
+     * -@c POSSIBLE_LOST_PACKET if there is a possiblity of a lost packets/frame
+     *     (end marker found without start marker / header found within frame). Read pointer is incremented.
+     */
+    ReturnValue_t checkForPackets(uint8_t* buffer,
+            size_t maxData, size_t* packetSize);
 
 private:
-	AnalyzerModes mode;
-	SharedRingBuffer* ringBuffer;
-	std::vector<uint8_t> analysisVector;
-	bool dataInAnalysisVectorLeft = false;
+    AnalyzerModes mode;
+    SharedRingBuffer* ringBuffer;
+    std::vector<uint8_t> analysisVector;
+    bool dataInAnalysisVectorLeft = false;
+    std::array<uint8_t, 3> uslpHeaderMarker;
+    // Stores VC Length map necessary for frame copying
+    std::map<uint8_t, size_t>* virtualChannelFrameSizes = nullptr;
 
-	size_t currentBytesRead = 0;
 
-	ReturnValue_t readRingBuffer();
-	ReturnValue_t handleDleParsing(uint8_t* receptionBuffer, size_t maxSize,
-				size_t* packetSize);
-	ReturnValue_t parseForDleEncodedPackets(size_t bytesToRead,
-			uint8_t* receptionBuffer, size_t maxSize,
-			size_t* packetSize, size_t* readSize);
+    size_t currentBytesRead = 0;
+
+    ReturnValue_t readRingBuffer();
+    ReturnValue_t handleParsing(uint8_t* receptionBuffer, size_t maxSize,
+                size_t* packetSize);
+    ReturnValue_t parseForDleEncodedPackets(size_t bytesToRead,
+            uint8_t* receptionBuffer, size_t maxSize,
+            size_t* packetSize, size_t* readSize);
+
+    ReturnValue_t parseForUslpFrames(size_t bytesToRead,
+            uint8_t* receptionBuffer, size_t maxSize,
+            size_t* packetSize, size_t* readSize);
 };
 
 
