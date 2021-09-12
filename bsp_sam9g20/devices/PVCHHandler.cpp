b2f01277289906a83419f3b1e1cdc9f3752bb1b8
Robin Mueller <robin.mueller.m@gmail.com>
2021-09-12 21:11:18 +0200

    maybe controller base is better here

--- bsp_sam9g20/devices/PVCHHandler.cpp
+++ bsp_sam9g20/devices/PVCHHandler.cpp
@@ -3,63 +3,63 @@
 
 #include "fsfw/devicehandlers/DeviceCommunicationIF.h"
 
-PVCHHandler::PVCHHandler(object_id_t objectId, object_id_t pvchWorkerId,
-		CookieIF * comCookie, uint8_t switchId):
-        DeviceHandlerBase(objectId, objects::SPI_DEVICE_COM_IF, comCookie),
-		pvchWorker(pvchWorkerId), switchId(switchId) {
+PVCHHandler::PVCHHandler(object_id_t objectId, object_id_t pvchWorkerId, uint8_t switchId):
+        /* DeviceHandlerBase(objectId, objects::SPI_DEVICE_COM_IF, comCookie), */
+        ExtendedControllerBase(objectId, objects::NO_OBJECT), pvchWorker(pvchWorkerId),
+        switchId(switchId) {
 }
 
 PVCHHandler::~PVCHHandler() {
 }
 
-void PVCHHandler::doStartUp() {
-}
-
-void PVCHHandler::doShutDown() {
-}
-
-ReturnValue_t PVCHHandler::buildNormalDeviceCommand(
-		DeviceCommandId_t *id) {
-	return RETURN_OK;
-}
-
-ReturnValue_t PVCHHandler::buildTransitionDeviceCommand(
-		DeviceCommandId_t *id) {
-	return RETURN_OK;
-}
-
-ReturnValue_t PVCHHandler::buildCommandFromCommand(
-		DeviceCommandId_t deviceCommand, const uint8_t *commandData,
-		size_t commandDataLen) {
-	return RETURN_OK;
-}
-
-void PVCHHandler::fillCommandAndReplyMap() {
-}
-
-ReturnValue_t PVCHHandler::scanForReply(const uint8_t *start,
-		size_t remainingSize, DeviceCommandId_t *foundId, size_t *foundLen) {
-	return RETURN_OK;
-}
-
-ReturnValue_t PVCHHandler::interpretDeviceReply(
-		DeviceCommandId_t id, const uint8_t *packet) {
-	return RETURN_OK;
-}
-
-void PVCHHandler::setNormalDatapoolEntriesInvalid() {
-}
-
-void PVCHHandler::debugInterface(uint8_t positionTracker, object_id_t objectId,
-		uint32_t parameter) {
-}
-
-uint32_t PVCHHandler::getTransitionDelayMs(Mode_t modeFrom,
-		Mode_t modeTo) {
-	return 5000;
-}
-
-ReturnValue_t PVCHHandler::getSwitches(const uint8_t **switches,
-		uint8_t *numberOfSwitches) {
-	return DeviceHandlerBase::NO_SWITCH;
-}
+//void PVCHHandler::doStartUp() {
+//}
+//
+//void PVCHHandler::doShutDown() {
+//}
+//
+//ReturnValue_t PVCHHandler::buildNormalDeviceCommand(
+//		DeviceCommandId_t *id) {
+//	return RETURN_OK;
+//}
+//
+//ReturnValue_t PVCHHandler::buildTransitionDeviceCommand(
+//		DeviceCommandId_t *id) {
+//	return RETURN_OK;
+//}
+//
+//ReturnValue_t PVCHHandler::buildCommandFromCommand(
+//		DeviceCommandId_t deviceCommand, const uint8_t *commandData,
+//		size_t commandDataLen) {
+//	return RETURN_OK;
+//}
+//
+//void PVCHHandler::fillCommandAndReplyMap() {
+//}
+//
+//ReturnValue_t PVCHHandler::scanForReply(const uint8_t *start,
+//		size_t remainingSize, DeviceCommandId_t *foundId, size_t *foundLen) {
+//	return RETURN_OK;
+//}
+//
+//ReturnValue_t PVCHHandler::interpretDeviceReply(
+//		DeviceCommandId_t id, const uint8_t *packet) {
+//	return RETURN_OK;
+//}
+//
+//void PVCHHandler::setNormalDatapoolEntriesInvalid() {
+//}
+//
+//void PVCHHandler::debugInterface(uint8_t positionTracker, object_id_t objectId,
+//		uint32_t parameter) {
+//}
+//
+//uint32_t PVCHHandler::getTransitionDelayMs(Mode_t modeFrom,
+//		Mode_t modeTo) {
+//	return 5000;
+//}
+//
+//ReturnValue_t PVCHHandler::getSwitches(const uint8_t **switches,
+//		uint8_t *numberOfSwitches) {
+//	return DeviceHandlerBase::NO_SWITCH;
+//}
--- bsp_sam9g20/devices/PVCHHandler.h
+++ bsp_sam9g20/devices/PVCHHandler.h
@@ -1,5 +1,5 @@
 #include <bsp_sam9g20/devices/pvch/PvchWorker.h>
-#include <fsfw/devicehandlers/DeviceHandlerBase.h>
+#include <fsfw/controller/ExtendedControllerBase.h>
 
 /**
  * @brief       Device Handler for the DLR PVCH device
@@ -10,32 +10,31 @@
  * @author      R. Mueller
  * @ingroup     devices
  */
-class PVCHHandler: public DeviceHandlerBase {
+class PVCHHandler: public ExtendedControllerBase {
 public:
-	PVCHHandler(object_id_t objectId, object_id_t pvchWorkerId, CookieIF * comCookie,
-			uint8_t switchId);
+	PVCHHandler(object_id_t objectId, object_id_t pvchWorkerId, uint8_t switchId);
 	virtual~ PVCHHandler();
 protected:
 	/* DeviceHandlerBase abstract function implementation */
-	void doStartUp() override;
-	void doShutDown() override;
-	ReturnValue_t buildNormalDeviceCommand(DeviceCommandId_t * id) override;
-	ReturnValue_t buildTransitionDeviceCommand(DeviceCommandId_t * id) override;
-	ReturnValue_t buildCommandFromCommand(DeviceCommandId_t deviceCommand,
-			const uint8_t * commandData, size_t commandDataLen) override;
-	void fillCommandAndReplyMap() override;
-	ReturnValue_t scanForReply(const uint8_t *start, size_t remainingSize,
-			DeviceCommandId_t *foundId, size_t *foundLen) override;
-	ReturnValue_t interpretDeviceReply(DeviceCommandId_t id,
-			const uint8_t *packet) override;
-	void setNormalDatapoolEntriesInvalid() override;
+//	void doStartUp() override;
+//	void doShutDown() override;
+//	ReturnValue_t buildNormalDeviceCommand(DeviceCommandId_t * id) override;
+//	ReturnValue_t buildTransitionDeviceCommand(DeviceCommandId_t * id) override;
+//	ReturnValue_t buildCommandFromCommand(DeviceCommandId_t deviceCommand,
+//			const uint8_t * commandData, size_t commandDataLen) override;
+//	void fillCommandAndReplyMap() override;
+//	ReturnValue_t scanForReply(const uint8_t *start, size_t remainingSize,
+//			DeviceCommandId_t *foundId, size_t *foundLen) override;
+//	ReturnValue_t interpretDeviceReply(DeviceCommandId_t id,
+//			const uint8_t *packet) override;
+//	void setNormalDatapoolEntriesInvalid() override;
 
 	/* DeviceHandlerBase overrides */
-	void debugInterface(uint8_t positionTracker = 0,
-			object_id_t objectId = 0, uint32_t parameter = 0) override;
-	uint32_t getTransitionDelayMs(Mode_t modeFrom, Mode_t modeTo) override;
-	ReturnValue_t getSwitches(const uint8_t **switches,
-			uint8_t *numberOfSwitches) override;
+//	void debugInterface(uint8_t positionTracker = 0,
+//			object_id_t objectId = 0, uint32_t parameter = 0) override;
+//	uint32_t getTransitionDelayMs(Mode_t modeFrom, Mode_t modeTo) override;
+//	ReturnValue_t getSwitches(const uint8_t **switches,
+//			uint8_t *numberOfSwitches) override;
 private:
 	PvchWorker pvchWorker;
 	const uint8_t switchId;
